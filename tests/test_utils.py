f72c8e421abc4a6243f137ff4d6f41aaa3deeda9
Krzysztof Nowak <k.nowak@cern.ch>
2018-03-13 11:29:13 +0100

    added tests for copyright date formatter
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

---
+++ tests/test_utils.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('..')
+from change_license import get_years_string
+import datetime
+
+
+def test_years_string_formatter():
+    """Test copyright years string formatter."""
+
+    curr_year = str(datetime.date.today().year)
+    assert get_years_string('2013') == '2015-2018'
+    assert get_years_string('2015') == '2015-2018'
+    assert get_years_string('2018') == '2018'
+    assert get_years_string('2019') == '2019'
+    assert get_years_string('2014', lower_year='2013', upper_year='2020') == \
+        '2014-2020'
+    assert get_years_string('2019', lower_year='2013', upper_year='2020') == \
+        '2019-2020'
+    assert get_years_string('2012', lower_year='2013', upper_year='2020') == \
+        '2013-2020'
