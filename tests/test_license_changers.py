47832eca0b3a490e7f15e02458353576f61a057e
Krzysztof Nowak <k.nowak@cern.ch>
2018-03-13 18:13:56 +0100

    refactored old parsing code
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- tests/test_license_changers.py
+++ tests/test_license_changers.py
@@ -1,19 +1,12 @@
-from change_license import change_license_for_jinja_content, change_license_for_js_content, change_license_for_html_content, change_license_for_rst_content, change_license_for_python_content
+from change_license import functs
 from textwrap import dedent
 import datetime
 
 
 def test_license_text_changers(example_contents):
     """Test license changers."""
-    fns = {
-        'jinja': change_license_for_jinja_content,
-        'js': change_license_for_js_content,
-        'html': change_license_for_html_content,
-        'rst': change_license_for_rst_content,
-        'python': change_license_for_python_content,
-    }
 
-    example_contents = [example_contents[15], ]
+    #example_contents = [example_contents[13], ]
     for idx, (filetype, in_text, exp_text) in enumerate(example_contents):
-        out_text = fns[filetype](in_text)
+        out_text, touched = functs[filetype](in_text)
         assert out_text == exp_text, "Failed at Idx: {0}".format(str(idx))
