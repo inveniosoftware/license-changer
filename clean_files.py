2255a817a283825144b358202959335fb342c792
Sebastian Witowski <witowski.sebastian@gmail.com>
2018-03-13 16:02:53 +0100

    Add clean_files.py script
    * Removes pytest-cache from setup.py files.

---
+++ clean_files.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+Clean some files:
+* Remove pytest-cache from setup.py file (as it's now part of pytest)
+
+Usage:
+  $ cd private/src/invenio-oaiserver
+  $ git checkout -b license-change
+  $ clean_files setup.py
+  $ git commit -a -s -m 'global: cleanup of dependencies'
+"""
+
+import click
+import os
+
+
+def clean_setup_file(filename):
+    """Clean the setup.py file."""
+    old_file = open(filename, 'r')
+    lines = old_file.readlines()
+    old_file.close()
+
+    new_file = open(filename, 'w')
+    for line in lines:
+        # pytest-cache is now part of pytest, so let's remove it
+        if "'pytest-cache>=1.0" not in line:
+            new_file.write(line)
+    new_file.close()
+
+
+@click.command()
+@click.argument('filename', type=click.Path(exists=True))
+def main(filename):
+    """Cleanup files."""
+    filename_basename = os.path.basename(filename)
+    if filename_basename == 'setup.py':
+        clean_setup_file(filename)
+
+
+if __name__ == '__main__':
+    main()
