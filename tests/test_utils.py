61e3545359c9259e386a3bef89e67bc0cde33ee6
Krzysztof Nowak <k.nowak@cern.ch>
2018-03-13 12:01:14 +0100

    fixed copyright date tests
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,15 +1,17 @@
+"""Utility functions tests."""
 from change_license import get_years_string
 import datetime
 
 
 def test_years_string_formatter():
     """Test copyright years string formatter."""
-
     curr_year = str(datetime.date.today().year)
-    assert get_years_string('2013') == '2015-2018'
-    assert get_years_string('2015') == '2015-2018'
-    assert get_years_string('2018') == '2018'
-    assert get_years_string('2019') == '2019'
+    assert get_years_string('2013') == '2015-' + curr_year
+    assert get_years_string('2015') == '2015-' + curr_year
+
+    # Single date if it's current year
+    assert get_years_string('2018') == curr_year
+    assert get_years_string('2030') == '2030'  # Date from future?
     assert get_years_string('2014', lower_year='2013', upper_year='2020') == \
         '2014-2020'
     assert get_years_string('2019', lower_year='2013', upper_year='2020') == \
