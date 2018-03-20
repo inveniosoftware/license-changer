e38e094e97d013872f030ecf04f0593a1405cea8
Sebastian Witowski <witowski.sebastian@gmail.com>
2018-03-20 18:46:39 +0100

    global: add function to update pypi passwords

--- tests/test_update_travis.py
+++ tests/test_update_travis.py
@@ -2,7 +2,7 @@ import filecmp
 import os
 from shutil import copyfile
 
-from update_travis import update_travis_config
+from update_travis import add_allow_failures, update_pypi_passwords
 
 
 def test_update_travis():
@@ -14,6 +14,10 @@ def test_update_travis():
 
     # Create the .travis.yml file from travis_in.yml
     copyfile(input_file, travis_file)
-    update_travis_config(travis_file)
+    add_allow_failures(travis_file)
+    # Update pypi passwords won't change the password in our dummy test, as
+    # there is no pypi password defined for license-changer repo.
+    # But it's ok, we can test that the username was changed
+    update_pypi_passwords(travis_file)
     assert filecmp.cmp(output_file, travis_file)
     os.remove(travis_file)
