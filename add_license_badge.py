ba11744b200e8237f72da600abf97d6a29623dfa
Krzysztof Nowak <k.nowak@cern.ch>
2018-03-23 17:10:02 +0100

    fix badge position to last place
    Signed-off-by: Krzysztof Nowak <k.nowak@cern.ch>

--- add_license_badge.py
+++ add_license_badge.py
@@ -7,12 +7,17 @@ def main():
     with open(filename, 'r') as fp:
         content = fp.read()
 
-    pos = content.find('.. image')
     pkg_name = re.search(r'(.*)(travis\/inveniosoftware\/)(.*)(.svg)(.*)', content).groups()[2]
-    fmt = (".. image:: https://img.shields.io/github/license/inveniosoftware/{pkg_name}.svg\n"
-           "        :target: https://github.com/inveniosoftware/{pkg_name}/blob/master/LICENSE\n\n")
-    shield = fmt.format(pkg_name=pkg_name)
-    new_out = content[:pos] + shield + content[pos:]
+    badge_lines = [
+        "",
+        ".. image:: https://img.shields.io/github/license/inveniosoftware/{pkg_name}.svg".format(pkg_name=pkg_name),
+        "        :target: https://github.com/inveniosoftware/{pkg_name}/blob/master/LICENSE".format(pkg_name=pkg_name),
+        ]
+
+    lines = content.split('\n')
+    last_img_idx = next(idx for idx, l in reversed(list(enumerate(lines))) if l.startswith('.. image'))
+    new_lines = lines[:last_img_idx+2] + badge_lines + lines[last_img_idx+2:]
+    new_out = "\n".join(new_lines)
 
     with open(filename, 'w') as fp:
         content = fp.write(new_out)
